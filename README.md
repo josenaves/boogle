6d4d2c269cb4c2a77335a7dbed79418b2a31704a
Jose Naves Moura Neto <josenaves@gmail.com>
2014-08-08 13:17:29 -0300

    Begining of integration tests

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ boogle
 
 A typical search engine written in Java8 and Spark
 
-curl -X POST -H "Content-Type: application/json" -d '{"content" : "This is a journey into sound", "pageId" : "1"}' http://localhost:8080/boogle/index
+To run and compile, you'll need Java8 and Gradle.
+
+To index a new page:
+curl -X POST -H "Content-Type: application/json" -d '{"content" : "This is a journey into sound", "pageId" : "1"}' http://localhost:4567/index
+
+To search:
+curl -X GET http://localhost:4567/search?q=you+can+search
+
---
+++ src/test/java/com/josenaves/boogle/IntegrationTest.java
@@ -0,0 +1,85 @@
+package com.josenaves.boogle;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import spark.Spark;
+import spark.utils.IOUtils;
+
+import com.google.gson.Gson;
+import com.josenaves.boogle.web.IndexController;
+
+public class IntegrationTest {
+
+	@BeforeClass
+	public static void beforeClass() throws InterruptedException {
+		IndexController.main(null);
+		Thread.sleep(1000);
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		Spark.stop();
+	}
+
+	@Test
+	public void pageMustBeIndexed() {
+		TestResponse res = request("POST", "/index", "{\"content\" : \"This is a journey into sound\", \"pageId\" : \"1\"}");
+
+		//Map<String, String> json = res.json();
+		assertEquals(201, res.status);
+		//assertNotNull(json.get("id"));
+	}
+
+	private TestResponse request(String method, String path, String requestBody) {
+		try {
+			URL url = new URL("http://localhost:4567" + path);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod(method);
+			connection.setDoOutput(true);
+			setJsonInRequestBody(connection, requestBody);
+			connection.connect();
+
+			String responseBody = IOUtils.toString(connection.getInputStream());
+			return new TestResponse(connection.getResponseCode(), responseBody);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+			fail("Sending request failed: " + e.getMessage());
+			return null;
+		}
+	}
+	
+	private void setJsonInRequestBody(HttpURLConnection connection, String json) throws IOException {
+		connection.setRequestProperty("Content-Type", "application/json");
+		byte[] outputInBytes = json.getBytes("UTF-8");
+		OutputStream os = connection.getOutputStream();
+		os.write(outputInBytes);
+		os.close();
+	}
+
+	private static class TestResponse {
+		public final String body;
+		public final int status;
+
+		public TestResponse(int status, String body) {
+			this.status = status;
+			this.body = body;
+		}
+
+		public Map<String, String> json() {
+			return new Gson().fromJson(body, HashMap.class);
+		}
+	}
+}
